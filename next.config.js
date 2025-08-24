06c64218f62b34872bef35c173f31673ebf1448b
Muneeb Ashraf <58017595+muneeb-ashraf@users.noreply.github.com>
2025-08-25 01:03:37 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+  serverExternalPackages: ["@sparticuz/chromium", "puppeteer-core"],
+};
 
-module.exports = nextConfig
+export default nextConfig;
