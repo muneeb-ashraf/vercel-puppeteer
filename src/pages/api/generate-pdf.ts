428c62b3b14dab6f7b16fcb79e6c2a59535e5b56
Muneeb Ashraf <58017595+muneeb-ashraf@users.noreply.github.com>
2025-08-29 18:46:00 +0500

    fixed

--- src/pages/api/generate-pdf.ts
+++ src/pages/api/generate-pdf.ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { createClient } from "@supabase/supabase-js";
-import puppeteer from "puppeteer";
+import puppeteer from "puppeteer-core";
 
 export default async function handler(
   req: NextApiRequest,
